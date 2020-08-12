a308c20dce96f385d6bb9b7493a9265d9be78ab6
Kostya Osipenko <osipenkokonstantin91@gmail.com>
2020-08-12 22:53:42 +0300

    hw

---
+++ script.js
@@ -0,0 +1,28 @@
+var runner = {
+    $$runnerId: null,
+    $$tasks: [],
+    setSpeed: function (speed) {
+        this.stop();
+        this.start(speed);
+    },
+    add: function (task) {
+        this.$$tasks.push(task);
+    },
+    start: function (speed) {
+        var _this = this;
+        this.$$runnerId = setTimeout(function callback() {
+            _this.$$tasks.forEach(function (value) {
+               value()
+            })
+            _this.$$runnerId = setTimeout(callback, speed);
+        }, speed);
+    },
+    stop: function () {
+        clearTimeout(this.$$runnerId);
+    }
+}
+runner.add(()=>{
+    console.log('hello');
+    console.log('world');
+});
+runner.setSpeed(2000);
